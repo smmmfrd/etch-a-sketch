3415d08166e3efe295b26af55bdaf5529ca83a76
Sam Mumford <smmmfrd@gmail.com>
2022-06-17 15:39:17 -0700

    Start CSS and Grid Building

---
+++ script.js
@@ -0,0 +1,19 @@
+const drawContainer = document.querySelector('#etch-a-sketch');
+
+function BuildGrid(){
+    let gridDimensions = 16;
+    let boxSize = 768 / gridDimensions;
+
+    for (let x = 0; x < gridDimensions; x++) {
+        for (let y = 0; y < gridDimensions; y++) {
+            CreateSquare(boxSize);
+        } 
+    }
+}
+
+function CreateSquare(pixelSize){
+    let sq = document.createElement('div');
+    sq.style=`width:${pixelSize}px;height:${pixelSize}px;background-color:white;`;
+    console.log(drawContainer);
+    drawContainer.appendChild(sq);
+}
